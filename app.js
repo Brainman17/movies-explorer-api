ec3373edf4ba12f4e5f7c4aaaaca41a288c7ab0c
Egor Andreychuk <jegor.andreichuk@yandex.ru>
2023-06-18 14:48:55 +0300

    fix: delete unnecessary linesЖ

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
 /* eslint-disable no-console */
 
 // Пакеты
